c076582ee4cd95fe0b6f4dfb4ea59bdb0925bd42
James McDermott <james.mcdermott7@mail.dcu.ie>
2021-01-09 15:10:36 +0000

    add git config via home-manager

--- users.nix
+++ users.nix
@@ -1,6 +1,14 @@
 { pkgs, ... }:
 {
-  users.extraUsers.root = {   
-    shell = pkgs.fish;
+  imports = [ <home-manager/nixos> ];
+  users.extraUsers.root = {
+    shell = pkgs.zsh;
+  };
+  home-manager.users.root = { pkgs, ... }: {
+    programs.git = {
+      enable = true;
+      userName  = "James McDermott";
+      userEmail = "james.mcdermott7@mail.dcu.ie";
+    };
   };
 }
