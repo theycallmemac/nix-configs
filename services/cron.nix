856f0e7f425a64a09c521542a5c40ada2deef019
James McDermott <james.mcdermott7@mail.dcu.ie>
2021-01-17 15:09:28 +0000

    add cron jobs to run garbage collection and optimisation on nix store

--- services/cron.nix
+++ services/cron.nix
@@ -1,5 +1,7 @@
 {
   services.cron.systemCronJobs = [
-    "0 12 * * 0 systemctl reload httpd.service" # pick up new ssl certs
+    "0 07 * * 7 nix-store --optimise" # seek out identical files in the nix store
+    "0 09 * * 7 nix-collect-garbage" # remove old unreference packages
+    "0 12 * * 7 systemctl reload httpd.service" # pick up new ssl certs
   ];
 }
